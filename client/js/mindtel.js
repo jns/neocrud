155b57923ac586b9cce36fa09831b05d1520b61c
shapiroj <joshua.shapiro@gmail.com>
2015-03-05 15:11:06 +0000

    Added support for adding labels inside show/:id

--- client/js/mindtel.js
+++ client/js/mindtel.js
@@ -44,13 +44,46 @@ function deleteRelationship(id) {
 	});
 }
 
-function addLabel(id) {
-	
-	$.ajax('label/'+id+"?"+query, {method: "PUT"}).done(function(){
-		document.location.reload();	
-	}).fail(function(error){
-		alert(error.responseText);
-	})
+function primaryNodeId() {
+	var nodeId =$('#nodeId');
+	if (typeof nodeId != undefined) {
+		return nodeId.html();
+	} else {
+		return undefined;
+	}
+}
+	function labelize(element, forNode) {
+		if ( typeof forNode == undefined) {
+			return;
+		}
+		element.css('display', 'inline');
+		var inp = $('<input class="contents hide" type="text" placeholder="label" />');
+		var but = $('<input id="expander" type="button" value="+" />');
+		var id = forNode;
+		element.append(inp);
+		element.append(but);
+		
+		but.click(function() {
+			inp.toggleClass('hide expand');
+		});
+		
+		inp.keypress(function(ev) {
+			if (ev.keyCode==13) {
+				var $this = $(this);
+				var value = $this[0].value;
+				$this[0].value = "";
+				$this.toggleClass('hide expand');
+				addLabel(id, value).done(function() {
+					element.before( $('<span class="label label-primary small margin-10"/>').text( value ) );
+				}).fail(function() {
+					console.log(err);
+				});
+			}
+		});
+	}
+function addLabel(id, label) {
+	var query = "l="+encodeURIComponent(label);
+	return $.ajax({url:'label/'+id+"?"+query,  method: "PUT"});
 }
 
 // constructs the suggestion engine
